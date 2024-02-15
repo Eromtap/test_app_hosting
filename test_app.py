de23c9997d94e6a9a16ce7424de490ab6496dbea
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 17:51:48 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import time
 
+pulse_1, pulse_2 =st.columns(2)
 
 numbers = st.empty()
 
+
 for i in range(100):
+
     conn = st.connection("postgresql", type="sql")
         
     df = conn.query('SELECT * FROM heart_rates;', ttl="0")
 
-    with numbers.container():
-        st.write(df)
-
+    with pulse_1:    
+        with numbers.container():
+            st.write(df)
+    with pulse_2:
+        with numbers.container():
+            st.write(df)        
 
 
     time.sleep(2)
