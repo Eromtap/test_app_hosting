6edad1fbca39d4db98cc7bb828c0b16d1a97f46d
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 17:56:03 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -11,13 +11,17 @@ for i in range(100):
     conn = st.connection("postgresql", type="sql")
         
     df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-
+    for row in df.itertuples():
+        p1 = row.pulse1
+        p2 = row.pulse2
+        
     with pulse_1:    
         with numbers.container():
-            st.write(df[0])
+            st.write(pulse1)
     with pulse_2:
         with numbers.container():
-            st.write(df[1])        
+            st.write(pulse2)
+     
 
 
     time.sleep(2)
