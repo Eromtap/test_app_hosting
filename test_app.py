6308dc707934a19807bf5f1866057803a7a41ef1
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 16:17:55 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT * FROM heart_rates;', ttl="0")
 
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"{row.pulse1} has a :{row.pulse2}:")
 
 
 st.write("hello")
