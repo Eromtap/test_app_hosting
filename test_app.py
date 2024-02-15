1a78e9ab0d36ff3f00216aeaadaedf12df650e62
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 15:58:00 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
-import sqlalchemy
-
-conn = st.connection("postgresql", type="sql")
 
 
-df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+#conn = st.connection("postgresql", type="sql")
 
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+
+#df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+
+#for row in df.itertuples():
+    #st.write(f"{row.name} has a :{row.pet}:")
+
+
+st.write("hello")
