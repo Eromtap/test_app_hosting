27bd40defa33e56d04456b6ab7512e1f2ad2b876
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 15:46:48 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,14 +1,9 @@
 import streamlit as st
-import psycopg2
+
+conn = st.connection("postgresql", type="sql")
 
 
-CONNECT = psycopg2.connect(
-    user = 'postgres',
-    password = 'booger123',
-    host = '34.171.140.16',
-    port = 5432,
-    database = 'summito2'
-    )
+df = conn.query('SELECT * FROM hert_rates;', ttl="0")
 
-
-st.write('app will live here... maybe')
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
