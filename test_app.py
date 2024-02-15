8989943f3d83e1946fb441d70cdb259705c6b8ae
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 18:24:47 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,27 +7,37 @@ pulse_1, pulse_2 = st.columns(2)
 
 conn = st.connection("postgresql", type="sql")
 
-for i in range(100):
+#for i in range(100):
 #numbers = st.empty()
 
-    df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-    for row in df.itertuples():
-        p1 = row.pulse1
-        p2 = row.pulse2
-        
-    with pulse_1: 
-        st.markdown("#### pulse 1")
-        numbers = st.empty()
+# df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+for row in df.itertuples():
+    p1 = row.pulse1
+    p2 = row.pulse2
+    
+with pulse_1: 
+    st.markdown("#### pulse 1")
+    numbers = st.empty()
+    while 1:
         with numbers.container():
-            st.write(p1)
-    with pulse_2:
-        st.markdown("#### pulse 2")   
-        numbers = st.empty()
-        with numbers.container():
-            st.write(p2)
-     
+                df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+                for row in df.itertuples():
+                p1 = row.pulse1
+                st.write(p1)
+        time.sleep(1)
+with pulse_2:
+    st.markdown("#### pulse 2")   
+    numbers = st.empty()
+    with numbers.container():
+        while 1:
+            with numbers.container():
+                    df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+                    for row in df.itertuples():
+                    p2 = row.pulse2
+                    st.write(p2)
+             time.sleep(1)
 
 
-    time.sleep(2)
+    # time.sleep(2)
 
 
