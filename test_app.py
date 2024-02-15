22d85f2b68690bafc84776b360bafcc6f37dc264
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 18:36:31 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,17 +7,9 @@ pulse_1, pulse_2 = st.columns(2)
 
 conn = st.connection("postgresql", type="sql")
 
-#for i in range(100):
-#numbers = st.empty()
-
-# df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-# for row in df.itertuples():
-#     p1 = row.pulse1
-#     p2 = row.pulse2
     
-with pulse_1, pulse_2: 
+with st.empty(): 
     st.markdown("pulse 1     pulse 2")
-    numbers = st.empty()
     while 1:
         with numbers.container():
                 df = conn.query('SELECT * FROM heart_rates;', ttl="0")
