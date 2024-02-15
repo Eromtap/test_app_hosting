71769175c65f1b4ba1e286bb5807676100230946
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 16:17:10 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,10 +4,10 @@ import streamlit as st
 conn = st.connection("postgresql", type="sql")
 
 
-#df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+df = conn.query('SELECT * FROM heart_rates;', ttl="0")
 
-#for row in df.itertuples():
-    #st.write(f"{row.name} has a :{row.pet}:")
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
 
 
 st.write("hello")
