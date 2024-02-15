93ef1e28899bfc315d82e893bcd199fbfe040076
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 15:27:34 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import psycopg2
+
+CONNECT = psycopg2.connect(
+    user = 'postgres',
+    password = 'booger123',
+    host = '34.171.140.16',
+    port = 5432,
+    database = 'summito2'
+    )
+
 
 st.write('app will live here... maybe')
