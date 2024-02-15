e22b7a54febbf6ee9c77b8b204d2992afab2f933
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 16:22:27 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,15 @@
 import streamlit as st
+import time
 
 
-conn = st.connection("postgresql", type="sql")
 
-
-df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-
-for row in df.itertuples():
-    st.write(f"{row.pulse1} has a :{row.pulse2}:")
+for i in range(100):
+    conn = st.connection("postgresql", type="sql")
+        
+    df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+    
+    for row in df.itertuples():
+        st.write(f"{row.pulse1} has a :{row.pulse2}:")
 
 
 st.write("hello")
