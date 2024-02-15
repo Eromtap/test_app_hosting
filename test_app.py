fd5d4d6b528980f3e8bce76bb78e8fd152743c6c
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 18:07:05 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -17,12 +17,12 @@ for i in range(100):
         
     with pulse_1: 
         st.markdown("#### pulse 1")
-        numbers = st.empty()
+        # numbers = st.empty()
         with numbers.container():
             numbers.write(p1)
     with pulse_2:
         st.markdown("#### pulse 2")   
-        numbers = st.empty()
+        # numbers = st.empty()
         with numbers.container():
             numbers.write(p2)
      
