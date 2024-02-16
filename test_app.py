b286f86329a3c39f8e58cac60ee0fd8f6f476747
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-16 08:21:09 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,36 +9,41 @@ conn = st.connection("postgresql", type="sql")
 
 left, right = st.columns(2)
 
+async def left_col():
+    with left:
+        st.markdown("#### Pulse Rate 1:")
+        numbers = st.empty()
+        
+        
+        for i in range(0,10):
+            df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+            for row in df.itertuples():
+                p1 = row.pulse1
+                # p2 = row.pulse2
+            with numbers.container():
+              st.write(p1)
+              # st.write(p2)
+              time.sleep(1)
 
-with left:
-    st.markdown("#### Pulse Rate 1:")
-    numbers = st.empty()
-    
-    
-    for i in range(0,10):
-        df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-        for row in df.itertuples():
-            p1 = row.pulse1
-            # p2 = row.pulse2
-        with numbers.container():
-          st.write(p1)
-          # st.write(p2)
-          time.sleep(1)
+async def right_col():
+    with right:
+        st.markdown("#### Pulse Rate 2:")
+        numbers = st.empty()
+        
+        
+        for i in range(0,10):
+            df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+            for row in df.itertuples():
+                p2 = row.pulse2
+            with numbers.container():
+              st.write(p2)
+              time.sleep(1)
 
 
-with right:
-    st.markdown("#### Pulse Rate 2:")
-    numbers = st.empty()
-    
-    
-    for i in range(0,10):
-        df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-        for row in df.itertuples():
-            p2 = row.pulse2
-        with numbers.container():
-          st.write(p2)
-          time.sleep(1)
 
+async def main():
+    await asyncio.gather(left_col(), right_col())
+    
 
 
 
