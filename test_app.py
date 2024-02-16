163d0c9a423977780250d5d3ff3f2bd322a1f2d2
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 19:03:56 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import time
 
-# pulse_1, pulse_2 = st.columns(2)
-
-# numbers = st.empty()
 
 conn = st.connection("postgresql", type="sql")
 
@@ -13,32 +10,20 @@ pulse_2 = ''
 empt = st.empty()
 
 with empt: 
-    st.markdown("####pulse 1     pulse 2")
     while 1:
-            df = conn.query('SELECT * FROM heart_rates;', ttl="1")
+            df = conn.query('SELECT * FROM heart_rates;', ttl="0")
             for row in df.itertuples():
                 p1 = row.pulse1
                 p2 = row.pulse2
-            st.write(p1, p2)
+                if p1 != pulse_1 and p2 != pulse_2:
+                    pulse_1 = p1
+                    pulse_2 = p2
+            st.write(pulse_1, pulse_2)
             time.sleep(2)
 
 
 
 
 
-# with pulse_2:
-#     st.markdown("#### pulse 2")   
-#     numbers = st.empty()
-#     with numbers.container():
-#         while 1:
-#             with numbers.container():
-#                     df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-#                     for row in df.itertuples():
-#                         p2 = row.pulse2
-#                     st.write(p2)
-#             time.sleep(1)
-
-
-    # time.sleep(2)
 
 
