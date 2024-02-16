5409fe4417339ce3543e264faa70942fb3636116
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 19:11:02 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -11,19 +11,21 @@ pulse_2 = ''
 
 empt = st.empty()
 
-with empt: 
-    while 1:
-        df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-        for row in df.itertuples():
-            p1 = row.pulse1
-            p2 = row.pulse2
-        with pulse1:
-            st.write(p1)
 
-        with pulse2:
-            st.write(p2)
-        
-        time.sleep(2)
+while 1:
+    df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+    for row in df.itertuples():
+        p1 = row.pulse1
+        p2 = row.pulse2
+    with pulse1:
+        st.empty()
+        st.write(p1)
+
+    with pulse2:
+        st.empty()
+        st.write(p2)
+    
+    time.sleep(2)
 
 
 
