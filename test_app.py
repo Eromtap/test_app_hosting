74eb6bb242d2749259a608a6363e4e2dabe1b39d
Mark Patmore <66063750+Eromtap@users.noreply.github.com>
2024-02-15 19:09:16 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -13,10 +13,10 @@ empt = st.empty()
 
 with empt: 
     while 1:
-            df = conn.query('SELECT * FROM heart_rates;', ttl="0")
-            for row in df.itertuples():
-                p1 = row.pulse1
-                p2 = row.pulse2
+        df = conn.query('SELECT * FROM heart_rates;', ttl="0")
+        for row in df.itertuples():
+            p1 = row.pulse1
+            p2 = row.pulse2
         with pulse1:
             st.write(p1)
 
